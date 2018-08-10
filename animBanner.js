760a5394963a887f3707226765603a35e48c9c6f
chris <chris@chrisjackem.com>
2018-08-10 13:48:18 -0700

    Ranges working
    Range string/int separator working

--- animBanner.js
+++ animBanner.js
@@ -24,7 +24,7 @@ window.onload = function(){
 	else css.appendChild(document.createTextNode(styles));
 	document.getElementsByTagName("head")[0].appendChild(css);
 
-	if (DEBUG) console.log("animBanner init success\n" + styles);
+	//if (DEBUG) console.log("animBanner init success\n" + styles);
 }
 
 
@@ -38,28 +38,60 @@ function init(){
 	}//f
 }
 
-function Okey(){
-	
-}
+
 
 function Animate( d, DATA ){
   var direction = "init";
-  var scroll = DATA[0].includes("scroll");
-  var fade = DATA[0].includes("fade");
+  var animType = DATA[0].toLowerCase();
+  var vals = DATA[1];
 
+  // 1 - animation type 
+  switch(animType){
 
-  // 0 - kind
-  if (scroll){
-  	direction = DATA[0].includes("X") ? "X" : "Y";
+  	case "scroll_x":
+  		direction = "x";
+
+  		break;
+
+  	case "scroll_y":
+		direction = "y";
+  		break;
+
+  	case "fade":
+
+  		break;
+
+  	case "rotate":
+
+  		break;
   }
 
-  console.log(DATA + " scroll " + scroll);
+  // 2 - Values
+  if (vals.length > 0){
+  	var valsSplit = vals.split("_");
+  	refineData(vals);
+  }
+
+  //console.log(DATA + " scroll " + scroll);
+}
+
+// Helpers ---------------------------------------------------------------
+
+function refineData(v){
+
+	var _split = v.split("_");
+	for (var i=0; i<_split.length; i++){
+		if (  !isNaN( Number(_split[i]) ) ){
+		 console.log( _split[i] + " number");
+		}else{
+			console.log( _split[i] + " str" );
+		}
+	}
 }
 
 function getDivData(d){
 	var da = d.getAttribute("data-anim");
-	if (da==null) return [null];
-	
+	if (da==null) return [null];	
 	var splitted = da.split(" ");
 	return splitted;
 }
