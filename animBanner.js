a6265bb00cd7e68c00f11ead8af33c2b4ef37fc5
chris <chris@chrisjackem.com>
2018-08-10 15:23:55 -0700

    Scroll
    Scroll happened. Food time

--- animBanner.js
+++ animBanner.js
@@ -7,7 +7,7 @@ var animDivs = []
 var css;
 var styles;
 
-var DEBUG = false;
+var DEBUG = true;
 
 window.onload = function(){
 
@@ -24,7 +24,7 @@ window.onload = function(){
 	else css.appendChild(document.createTextNode(styles));
 	document.getElementsByTagName("head")[0].appendChild(css);
 
-	//if (DEBUG) console.log("animBanner init success\n" + styles);
+	if (DEBUG) console.log("animBanner init success\n" + styles);
 }
 
 
@@ -41,9 +41,8 @@ function init(){
 
 
 function Animate( d, DATA ){
-  var direction = "init";
   var animType = DATA[0].toLowerCase();
-  var vals = DATA[1];
+  var vals = DATA[1].split("_");
   var dataStr = "";
 
   var str_property = "background-color";
@@ -52,12 +51,19 @@ function Animate( d, DATA ){
 
   // animation type 
   switch(animType){
-
-  	case "scroll_x":
+  	
   	case "scroll_y":
-  		direction = (animType=="scroll_x")?"x":"y";
-  		str_property = "background-position";
-  		str_start = "background-position";
+  		embedToBG(d);
+ 		str_property = "background-position";
+  		str_start = "0px " + vals[0];
+  		str_finish = "0px " + vals[1] ;
+  		break;
+  	
+  	case "scroll_x":
+  		embedToBG(d);
+ 		str_property = "background-position";
+  		str_start = vals[0] + " 0px";
+  		str_finish = vals[1] + " 0px";
   		break;
 
   	case "fade":
@@ -72,9 +78,41 @@ function Animate( d, DATA ){
   	 console.log("Error - cant find animation " + animType);
   	 break;
   }//s
+
+  var _ID = d.getAttribute('id');
+  styles += "\n#" + _ID + "{animation-name: " + _ID + ";\n";
+  styles += "animation-duration: " + "1s" + ";}\n";
+  styles += parseCss( _ID, str_property, str_start, str_finish );
+
+
 }
 
 // Helpers ---------------------------------------------------------------
+function embedToBG(par){
+	var imgChild = par.getElementsByTagName("img")[0];
+	var imgStyle = 'background-image: url("' + refineSource(imgChild.src) +'"); ';
+		imgStyle +="height:" + imgChild.clientHeight + "px; ";
+	// Add to secondary css that we are building
+	styles += "#" + par.getAttribute('id') + "{" + imgStyle + "}";
+	par.removeChild(imgChild);
+}
+
+
+
+function refineSource(s){
+	var _spl = s.split("/");
+	var _file = _spl[_spl.length-1];
+	var _fol =  _spl[_spl.length-2];
+	return _fol + "/" + _file;
+}
+
+function parseCss( name, prop, sta, fin ){
+	var ret = "\n@keyframes " + name + "{";
+		ret += "from {"+ prop + ":" + sta + ";}\n";
+		ret += "to {"+ prop + ":" + fin + ";} }\n";// closer
+		return ret;
+		
+}
 
 function getDivData(d){
 	var da = d.getAttribute("data-anim");
