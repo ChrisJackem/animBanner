2b2f421ec41c2bdbafbcfeaa691f116606b0715f
Chris Jackem <chrisjackem@gmail.com>
2024-10-26 09:21:05 -0400

    Datatype Update
    ES6 update

--- animBanner.js
+++ animBanner.js
@@ -5,7 +5,7 @@ var DEBUG = true;
 var curZ = 10;
 var master = {};
 
-var defaults = {
+const defaults = {
 // custom
   'background-image':'none',
   'height':'100%',
@@ -24,7 +24,7 @@ var defaults = {
   'animation-play-state': 'running'
 };
 
-var exclude_from_css = {
+const exclude_from_css = {
   'animation-prop':'X',
   'animation-end':'X',
   'animation-start':'X'
@@ -34,15 +34,15 @@ window.onload = function(){
 	init();
 
 	// New styles for animations
-	var css = document.createElement("style");
+	let css = document.createElement("style");
 	css.type = "text/css";
-	var styles = '';
+	let styles = '';
 
-	for (var obj in master){		
-		var _obj = master[obj];		
+	for (let obj in master){		
+		let _obj = master[obj];		
 		styles = '\n#' + _obj['animation-name'] + '{';
-		for (var prop in _obj){
-			var val = _obj[prop];
+		for (let prop in _obj){
+			let val = _obj[prop];
 			if (!exclude_from_css.hasOwnProperty(prop)) styles += prop + ':' + val + ';';
 		}
 		styles += "}" + parseCss(_obj['animation-name'],
@@ -57,23 +57,22 @@ window.onload = function(){
 	}
 	// Add css to doc
 	document.getElementsByTagName("head")[0].appendChild(css);
-
 }
 
 function init(){	
-	var animDivs = Array.prototype.slice.call(document.getElementsByTagName('*'));
-	for (var a in animDivs){
-		var _D = getDivData(animDivs[a]);
+	let animDivs = Array.prototype.slice.call(document.getElementsByTagName('*'));
+	for (let a in animDivs){
+		let _D = getDivData(animDivs[a]);
 		if ((typeof _D[0]) == "string") Animate(animDivs[a], _D);
 	}
 }
 
 function Animate( d, DATA ){
 
-  var animType = DATA[0].toLowerCase();
-  var vals = DATA[1].split("_");
-  var _ID = d.getAttribute('id');
-  var myObj = (_ID in master) ? master[_ID] : {};
+  let animType = DATA[0].toLowerCase();
+  let vals = DATA[1].split("_");
+  let _ID = d.getAttribute('id');
+  let myObj = (_ID in master) ? master[_ID] : {};
 
   //myObj = Object.assign(defaults);
   myObj['animation-name'] = _ID;
@@ -86,11 +85,11 @@ function Animate( d, DATA ){
 	case "scroll_y":  		
 
   		// Replace image with div and transfer atts (you will never know)
-  		var imgParent = document.getElementById(_ID).parentElement;
-  		var imgHeight = d.clientHeight;
-  		var imgWidth = d.clientWidth;  		
-  		var imgSrc = refineSource(d.src);
-  		var imgDiv = document.createElement("div");
+  		let imgParent = document.getElementById(_ID).parentElement;
+  		let imgHeight = d.clientHeight;
+  		let imgWidth = d.clientWidth;  		
+  		let imgSrc = refineSource(d.src);
+  		let imgDiv = document.createElement("div");
   			if (imgDiv.className != null) imgDiv.className = d.className;  			
   			imgParent.replaceChild(imgDiv, document.getElementById(_ID) );
   			imgDiv.id = _ID;
@@ -164,18 +163,17 @@ function Animate( d, DATA ){
 
 // Helpers ---------------------------------------------------------------
 function refineSource(s){
-	var _spl = s.split("/");
-	var _file = _spl[_spl.length-1];
-	var _fol =  _spl[_spl.length-2];
+	let _spl = s.split("/");
+	let _file = _spl[_spl.length-1];
+	let _fol =  _spl[_spl.length-2];
 	return _fol + "/" + _file;
 }
 
 function parseCss( name, prop, sta, fin ){
-	var ret = "\n@keyframes " + name + "{";
+	let ret = "\n@keyframes " + name + "{";
 		ret += "from {"+ prop + ":" + sta + ";}\n";
 		ret += "to {"+ prop + ":" + fin + ";} }\n";// closer
-		return ret;
-		
+		return ret;		
 }
 
 function getDivData(d){
